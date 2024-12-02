bc6c2f6891e0412a5b804e8a2888d032508a3941
Ryan Baggs <ryanbaggs7@gmail.com>
2024-12-02 16:06:39 -0600

    Solution to day1 part two
    Signed-off-by: Ryan Baggs <ryanbaggs7@gmail.com>

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,49 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
 pub(crate) fn day1() {
+    part_one();
+    let (left_list, right_list) = build_lists();
+    part_two(left_list, right_list);
+}
+
+fn part_one() {
     let (mut left_list, mut right_list) = build_lists();
 
     left_list.sort();
     right_list.sort();
 
     print_distance(left_list, right_list);
+}
+
+fn part_two(left_list: Vec<u32>, right_list: Vec<u32>) {
+    let mut counters = HashMap::new();
+
+    // Count the number of times ID number shows in the right list
+    // O(n) time complexity
+    for id_num in right_list {
+        if let Some(counter) = counters.get_mut(&id_num) {
+            *counter += 1;
+        } else {
+            counters.insert(id_num, 1);
+        }
+    }
+
+    // Get the similarity score for each ID number in left list
+    // O(n) time complexity
+    let mut similarity_scores = Vec::new();
+    for id_num in left_list {
+        if counters.contains_key(&id_num) {
+            let similarity_score = id_num * counters.get(&id_num)
+                .expect("Verified it exists");
+            similarity_scores.push(similarity_score);
+        }
+    }
+    
+    let sum: u32 = similarity_scores.iter().sum();
+    println!("Similarity score of the lists is: {}", sum);
 }
 
 fn build_lists() -> (Vec<u32>, Vec<u32>){
