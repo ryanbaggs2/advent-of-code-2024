d45eb847afe90e878f4b2679b2b4611a01bdc104
Ryan Baggs <ryanbaggs7@gmail.com>
2024-12-04 20:03:39 -0600

    I'm stuck :(
    Signed-off-by: Ryan Baggs <ryanbaggs7@gmail.com>

--- src/day2.rs
+++ src/day2.rs
@@ -1,9 +1,11 @@
 use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::cmp;
 
 pub fn day2() {
     part_one();
+    part_two();
 }
 
 // Worst case O(n) time complexity
@@ -18,6 +20,21 @@ fn part_one() {
     }
     
     println!("Number of safe reports: {}", safe_reports_count);
+}
+
+fn part_two() {
+    let mut safe_reports_count = 0;
+    let reports = parse_reports();
+
+    for mut report in reports {
+        if safe_p2(&mut report) {
+            safe_reports_count += 1;
+        }
+    }
+    println!(
+        "Number of safe reports with 1 bad level or less: {}",
+        safe_reports_count
+    );
 }
 
 // O(n) time complexity
@@ -88,4 +105,44 @@ fn safe_recursive(report: &mut VecDeque<i8>, increasing: bool) -> bool {
     report.pop_front();
     
     safe_recursive(report, increasing)
+}
+
+// O(n) time complexity
+fn safe_p2(report: &mut VecDeque<i8>) -> bool {
+    let mut previous_level = report[0];
+    report.pop_front();
+    
+    let mut differences: Vec<i8> = Vec::new();
+    let mut positive_quantity = 0;
+    let mut negative_quantity = 0;
+    let mut bad_levels = 0;
+    
+    for current_level in report {
+        differences.push(previous_level - *current_level);
+        previous_level = *current_level;
+    }
+    
+    for diff in differences {
+        if diff == 0 || diff.abs() > 3 {
+            bad_levels += 1;
+            // It's already a bad level, we don't need to include it in the increasing or
+            // decreasing checks
+            continue;
+        }
+        if diff > 0 {
+            positive_quantity += 1;
+        }
+        if diff < 0 {
+            negative_quantity += 1;
+        }
+    }
+    
+    // Include the bad levels caught for increasing/decreasing
+    bad_levels = bad_levels + cmp::min(positive_quantity, negative_quantity);
+    
+    if bad_levels > 1 {
+        return false
+    }
+    
+    true
 }
